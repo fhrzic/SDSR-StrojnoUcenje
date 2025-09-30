da8092cf39c7cbf57d7d65a49c9a990d3bc9c60d
fhrzic <franko.hrzic@uniri.hr>
2025-09-30 11:34:22 +0200

    environment build-up

---
+++ Environment/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="SDSR-SU",
+    version="0.1.0",
+    description="Code for course: Machine Learning (SDSR)",
+    author="Your Name",
+    author_email="franko.hrzic@uniri.hr",
+    packages=find_packages(),  
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pandas",
+        "jupyterlab",
+        "notebook"       
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+
+    python_requires=">=3.12",
+)
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Službeni repozitorij kolegija strojno učenje.
 Izvođači:
 
   * prof. dr. sc. Ivan Štajduhar
-  * dr. sc. Franko Hržić
+  * dr. sc. Franko Hržić (franko.hrzic@uniri.hr)
 
 Laboratorij za umjetnu inteligenciju Tehničkog fakulteta Sveučilišta u Rijeci
 
@@ -13,5 +13,8 @@ Laboratorij za umjetnu inteligenciju Tehničkog fakulteta Sveučilišta u Rijeci
 ## Termin održavanja
 
 Predavanja: Utorkom od 8:30 do 10:00, u učionici U9.
+
 Vježbe: Četvrtkom od 16:00 do 18:00, u informatičkom kabinetu I8.
+
+---
 
